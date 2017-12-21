b2ff2a2a7dcd1cc937e89f45d2ec771e25ee83ef
Maqsood <maqsoodh19@gmail.com>
2017-12-21 23:44:14 +0500

    MAD Assignment

---
+++ app/src/main/java/com/example/maqso/assignment04/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.maqso.assignment04;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtLimitInput;
+    TextView txtLimitOutput;
+    Button btnStart, btnStop;
+    Intent intent;
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EventBus.getDefault().register(this);
+        intent = new Intent(this, CounterService.class);
+        txtLimitInput = findViewById(R.id.txt_limit_input);
+        txtLimitOutput = findViewById(R.id.txt_limit_output);
+        btnStart = findViewById(R.id.btn_ServiceStart);
+        btnStop = findViewById(R.id.btn_service_stop);
+        btnStop.setEnabled(false);
+        //start service
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (txtLimitInput.getText().toString().isEmpty()) {
+                    txtLimitInput.setError("Limit Needed");
+                } else {
+                    int value = Integer.parseInt(txtLimitInput.getText().toString());
+                    intent.putExtra("limit", value);
+                    startService(intent);
+                    btnStart.setEnabled(false);
+                    btnStop.setEnabled(true);
+                }
+            }
+        });
+        //stop servive
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(intent);
+                btnStart.setEnabled(true);
+                btnStop.setEnabled(false);
+            }
+        });
+    }
+
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEvent(MyEvents event) {
+        txtLimitOutput.setText(event.getValue() + " %");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/example/maqso/assignment04/MyEvents.java
@@ -0,0 +1,17 @@
+package com.example.maqso.assignment04;
+
+/**
+ * Created by maqso on 12/21/2017.
+ */
+
+public class MyEvents {
+    int value;
+
+    public MyEvents(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
